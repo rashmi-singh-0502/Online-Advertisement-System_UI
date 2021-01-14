b63394f22846789bfe9a7e70c65e5f0a285a75cc
mahima jain <jainmahima0906@gmail.com>
2021-01-14 20:41:01 +0530

    ViewCategoryList

--- src/components/admin-services.js
+++ src/components/admin-services.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
+import ViewCategoryListComponent from '../components/admincomponents/view_category_list'
 
 const AdminServicesComponent = (props)=>{
     return (
@@ -9,6 +10,11 @@ const AdminServicesComponent = (props)=>{
                 <Tab eventKey="Admin add Category" title="Add Category"> 
                    
                 </Tab>
+                <Tab eventKey="View Category List" title="View Category List"> 
+                <ViewCategoryListComponent></ViewCategoryListComponent>
+                </Tab>
+
+
                 <Tab eventKey="User List" title="View User List">
                   
                 </Tab>
--- src/reducers/advertise/global-reducer.js
+++ src/reducers/advertise/global-reducer.js
@@ -3,7 +3,13 @@ const globalReducer = (state = initialState, action) => {
     switch(action.type){
         case 'GET_ADVERTISES':
             return action.payload;
-        default:
+            /*case 'ADD_CATEGORY':
+                console.log("category add in reducer",action.payload);
+                return action.payload;*/
+                case 'SHOW_CATEGORY_LIST':
+                    console.log("category view in reducer",action.payload);
+                    return action.payload;
+            default:
             return [];
     }
 }
