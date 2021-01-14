74ca46f2a2c6df2ac354a1126bf45397f78147cd
vishakhadhote7 <vishakhadhote7@gmail.com>
2021-01-14 19:24:49 +0530

    commited now

--- src/reducers/advertise/global-reducer.js
+++ src/reducers/advertise/global-reducer.js
@@ -3,6 +3,9 @@ const globalReducer = (state = initialState, action) => {
     switch(action.type){
         case 'GET_ADVERTISES':
             return action.payload;
+
+            case 'SEARCH_ADVERTISE': 
+            return action.payload;
         default:
             return [];
     }
